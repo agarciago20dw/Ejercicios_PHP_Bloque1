077d51158d34a05dff51317eb63876ed7537afc3
AdrianDW32 <agarciago20dw@ikzubirimanteo.com>
2021-10-06 10:48:06 +0200

    Modificado Comunidad.php (formulario)

--- Comunidad.php
+++ Comunidad.php
@@ -1,3 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comunidad</title>
+</head>
+<body>
+    <form method="get">
+        <table style="text-align: center;">
+            <tr>
+                <td><label>Nº pisos</label></td>
+                <td><input type="text" name="pisos" ></td>
+            </tr>
+            <tr>
+                <td><label>Nº puertas</label></td>
+                <td><input type="text" name="puertas" ></td>
+            </tr>
+            <tr>
+                <td colspan="2"><input type="submit" value="ENVIAR" style="margin-top: 10px;;"></td>
+            </tr>
+        </table> 
+    </form>
+    <br>
+</body>
+</html>
+
 <?php
     class Comunidad {
 
@@ -16,11 +44,19 @@
             return $this->num_pisos * $this->num_puertas;
         }
 
+        public function mostrarViviendas() {
+            
+        }
+
     }
 
-    $num_pisos = rand(1, 100);
-    $num_puertas = rand(1, 5);
 
-    $comunidad1 = new Comunidad($num_pisos, $num_puertas);
-    echo "<br><br>En esta comunidad hay ".$comunidad1->calcularViviendas()." viviendas";
+    if ((isset($_GET["pisos"])) && (isset($_GET["puertas"]))) {
+        $num_pisos = $_GET["pisos"];
+        $num_puertas = $_GET["puertas"];
+
+        $comunidad1 = new Comunidad($num_pisos, $num_puertas);
+        echo "<br><br>En esta comunidad hay ".$comunidad1->calcularViviendas()." viviendas";
+    }
+    
 ?>
